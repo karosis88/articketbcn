8ea982e1de495e9b3164319437902912b90f8ce6
Karen Petrosyan <kar.petrosyan03@gmail.com>
2021-10-11 20:17:06 +0400

    firstsectionleft

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 alert(window.innerWidth)
+const maintitle = document.querySelector(".maintitle");
+const header = document.querySelector(".header");
+mini = false
+function scrollign ()
+{
+    let screenpos = maintitle.getBoundingClientRect()
+    console.log(screenpos.top)
+    if (screenpos.top < 80) {
+        if (!mini) {
+            mini = true
+            if (window.innerWidth < 1920) {header.classList.toggle('headermini')}
+            else {header.classList.toggle('secondheadermini')}
+        }
+    }
+
+    else {
+        if (mini) {
+            mini = false
+            if (window.innerWidth < 1920) {header.classList.toggle('headermini')}
+            else {header.classList.toggle('secondheadermini')}
+        }
+    }
+}
+
+document.addEventListener('scroll', scrollign)
